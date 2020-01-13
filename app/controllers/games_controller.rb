8a137924a5bd58fe8827dd1f045bc13c451ada45
Tolan Thornton <taoantaoan@hotmail.com>
2020-01-13 11:22:08 -0700

    added styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
   def new
+    abc = ('A'..'Z').to_a
+    word_length = rand(4..10)
+
+    @letters = []
+    word_length.times { @letters.push(abc.sample()) }
   end
 
   def score
